cbdd4c1e47ca749a5e45a53fb4bfa80d3058bd96
daria <daria@host.docker.internal>
2024-06-02 23:53:06 +0300

    Person-service

---
+++ person-sevice/src/main/java/telran/java52/PersonServiceApplication.java
@@ -0,0 +1,11 @@
+package telran.java52;
+
+import org.springframework.boot.SpringApplication;
+
+public class PersonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonServiceApplication.class, args);
+	}
+
+}
