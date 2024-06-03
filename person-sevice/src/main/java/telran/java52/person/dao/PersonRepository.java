9eeec98bc5ceed412736dd9dbf9d5f48e7ddfe59
daria <daria@host.docker.internal>
2024-06-03 20:14:00 +0300

    Updates

--- person-sevice/src/main/java/telran/java52/person/dao/PersonRepository.java
+++ person-sevice/src/main/java/telran/java52/person/dao/PersonRepository.java
@@ -1,9 +1,25 @@
 package telran.java52.person.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import telran.java52.person.dto.CityPopulationDto;
 import telran.java52.person.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 
+	@Query("select p from Citizen p where p.name=?1")
+	Stream<Person> findByNameIgnoreCase(String name);
+		
+	@Query("select p from Citizen p where p.address.city=:cityName")
+	Stream<Person> findByAddressCityIgnoreCase(String city);
+	
+	Stream<Person> findByBirthDateBetween(LocalDate from, LocalDate to);
+	
+	@Query ("select new telran.java52.person.dto.CityPopulationDto(p.address.city, count(p)) from Citizen p group by p.address.city order by count(p) desc")
+	List<CityPopulationDto> getCitiesPopulation();
 }
--- person-sevice/src/main/java/telran/java52/person/service/PersonService.java
+++ person-sevice/src/main/java/telran/java52/person/service/PersonService.java
@@ -1,10 +1,26 @@
 package telran.java52.person.service;
 
+import telran.java52.person.dto.AddressDto;
+import telran.java52.person.dto.CityPopulationDto;
 import telran.java52.person.dto.PersonDto;
 
 public interface PersonService {
 	Boolean addPerson(PersonDto personDto);
 	
 	PersonDto findPersonById(Integer id);
+
+	PersonDto updatePersonName(Integer id, String name);
+
+	PersonDto updatePersonAddress(Integer id, AddressDto addressDto);
+
+	PersonDto[] findPersonsByCity(String city);
+
+	PersonDto[] findPersonsByName(String name);
+
+	PersonDto[] findPersonsBetweenAge(Integer minAge, Integer maxAge);
+
+	Iterable<CityPopulationDto> getCitiesPopulation();
+
+	PersonDto removePerson(Integer id);
 
 }
